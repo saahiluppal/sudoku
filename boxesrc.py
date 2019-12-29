3c40f4934b3c50cf36ea9e6dc731150de0b8c16f
Sahil Uppal <sahiluppal2k@gmail.com>
2019-12-29 12:29:35 +0530

    clf2 ref added

--- boxesrc.py
+++ boxesrc.py
@@ -9,7 +9,7 @@ def rect(img, intersections):
     image = cv2.adaptiveThreshold(
         image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 101, 1)
 
-    clf = keras.models.load_model('classifier.h5')
+    clf = keras.models.load_model('classifier_new.h5')
     font = cv2.FONT_HERSHEY_SIMPLEX
 
     img2 = img.copy()
