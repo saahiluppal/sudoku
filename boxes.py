c6ea5790105a8d516dc459b8efe6314fcd5085bc
Sahil Uppal <sahiluppal2k@gmail.com>
2020-01-03 16:07:13 +0530

    stabalized version

--- boxes.py
+++ boxes.py
@@ -1,14 +1,15 @@
+import warnings
+warnings.filterwarnings('ignore')
 import numpy as np
 import cv2
-import joblib
-
+import tensorflow.keras as keras
 
 def rect(img, intersections):
     image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     image = cv2.adaptiveThreshold(
         image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 101, 1)
 
-    clf = joblib.load('classifier.pkl')
+    clf = keras.models.load_model('classifier2.h5')
     font = cv2.FONT_HERSHEY_SIMPLEX
 
     img2 = img.copy()
@@ -23,36 +24,33 @@ def rect(img, intersections):
             # cv2.imwrite('/home/sahiluppal/sudoku/vals/'+str((i+1)*(j+1))+'.bmp',img[y1:y2,x1:x2])
             cv2.rectangle(img2, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
-    while True:
-        cv2.imshow('image',img2)
-        if cv2.waitKey(1) & 0xFF==27:
-            break
-    cv2.destroyAllWindows()
-
     prediction = list()
-    if input('is it right?') == 'y':
-        for i in range(9):
-            for j in range(9):
-                y1 = int(intersections[j + i * 10][1] + 5)
-                y2 = int(intersections[j + i * 10 + 11][1] - 5)
-                x1 = int(intersections[j + i * 10][0] + 5)
-                x2 = int(intersections[j + i * 10 + 11][0] - 5)
-                X = image[y1:y2,x1:x2]
+    for i in range(9):
+        for j in range(9):
+            y1 = int(intersections[j + i * 10][1] + 5)
+            y2 = int(intersections[j + i * 10 + 11][1] - 5)
+            x1 = int(intersections[j + i * 10][0] + 5)
+            x2 = int(intersections[j + i * 10 + 11][0] - 5)
+            X = image[y1:y2,x1:x2]
 
-                if(X.size != 0):
-                    X = cv2.resize(X, (36, 36))
-                    num = clf.predict(np.reshape(X, (1, -1)))
-                    prediction.append(num[0])
-
-    else:
-        exit()
+            if(X.size != 0):
+                X = cv2.resize(X, (28, 28))
+                X = X/255.0
+                X = np.resize(X, (28,28,1))
+                try:
+                #num = clf.predict(np.resize(X,(1,28,28,1)))
+                    num = clf.predict(np.reshape(X, (1, 28, 28, 1)))
+                    prediction.append(np.argmax(num[0]) if np.argmax(num[0])!=10 else 0)
+                except: 
+                    prediction.append(0)
+                #print(num[0])
 
     if len(prediction) != 81:
-        print('not found 81 elements')
-        print('try again')
-        exit()
+        print(prediction)
+    else:
+        prediction = np.reshape(prediction,(9,9)).T
+        print(prediction)
+        return img, prediction, True
 
-    prediction = np.reshape(prediction,(9,9)).T
-    print(prediction)
 
-    return img,prediction
+    return img,prediction, False
